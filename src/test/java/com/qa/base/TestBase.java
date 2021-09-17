b761c38dde165589129a811987cace158bb7ae61
Ayan Bhattacharjee <ayan.bhattacharjee122015@gmail.com>
2021-09-17 11:57:42 +0530

    "4th commit"

--- src/test/java/com/qa/base/TestBase.java
+++ src/test/java/com/qa/base/TestBase.java
@@ -1,5 +1,35 @@
-package com.qa.base;
 
-public class TestBase {
+	package com.qa.base;
 
-}
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.annotations.Parameters;
+
+
+
+
+
+
+
+
+
+	public class TestBase {
+		public static WebDriver driver;
+		
+		 
+		@Parameters({"url"})
+		
+		public void initialization(String url)
+		{
+			System.setProperty("webdriver.chrome.driver","C://Users//Dell//Downloads//chromedriver_win32//chromedriver.exe");
+			driver=new ChromeDriver();
+			
+			
+			driver.manage().window().maximize();
+			driver.get(url);
+		}
+	 
+	}
+
+
+
